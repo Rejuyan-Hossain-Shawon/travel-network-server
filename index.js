9998832448080a18d4637d5c2c29d2fba02d8929
ukwl <ukwldeveloper02@gmail.com>
2021-11-01 09:20:54 +0600

    heroku server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
 const app = express()
 const cors = require("cors")
 require('dotenv').config()
@@ -10,9 +11,37 @@ app.use(cors());
 app.use(express.json());
 
 
+// database connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7niub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("shawon_travel-network");
+        const tourProgramsCollection = database.collection("tour_programs");
+
+        // get all the tour programs from database
+        app.get("/programs", async (req, res) => {
+            const cursor = tourProgramsCollection.find({});
+            const tourPrograms = await cursor.toArray();
+            res.json(tourPrograms);
+        })
+
+
+    }
+    finally {
+        // await client.close()
+    }
+
+
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('shawon travel network server is running')
 })
 
 app.listen(port, () => {
